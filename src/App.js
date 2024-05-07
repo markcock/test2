748cb1f1fb4a6f478bf0dcebb1abe25ce75090e5
Mark <markcj.paclibar@students.isatu.edu.ph>
2024-05-07 19:39:07 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Clipped from './drawer';
+
+import Drawer from './drawer';
+
+function App() {
+  return (
+    <div className="App">
+    <Clipped />
+    
+    </div>
+  );
+}
+
+export default App;
